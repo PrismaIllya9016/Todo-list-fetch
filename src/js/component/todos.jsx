79f799db1ef244ccb420ba78c1b56c44a48096bc
metantonio <antonio_martinez88@hotmail.com>
2022-10-04 03:10:19 +0000

    updt

--- src/js/component/todos.jsx
+++ src/js/component/todos.jsx
@@ -9,7 +9,7 @@ export const Todo = () => {
 
   const crearUsuario = async () => {
     let URI = `${BASE_URL}user/${usuario}`;
-    
+
     try {
       let respuesta = await fetch(URI, {
         method: "POST",
@@ -22,39 +22,70 @@ export const Todo = () => {
         return;
       } else {
         alert("Error al crear usuario, o el usuario ya existe");
-        
       }
       let respuestaJSON = await respuesta.json();
-      console.log(respuestaJSON)
-
+      console.log(respuestaJSON);
     } catch {
       (e) => console.log(e);
     }
   };
 
-  const traerListaTareas = async() => {
+  const traerListaTareas = async () => {
     let URI = `${BASE_URL}user/${usuario}`;
-    try{
-      let respuesta = await fetch(URI)
-      if(respuesta.ok){
-        let respuestaJSON = await respuesta.json()
-        console.log(respuestaJSON)
-        console.log(listTodos)
-        setListTodos(respuestaJSON)
-        console.log(listTodos)
-      }else{
-        console.log("respuesta fallida")
-        setListTodos([])
+    try {
+      let respuesta = await fetch(URI);
+      if (respuesta.ok) {
+        let respuestaJSON = await respuesta.json();
+        console.log(respuestaJSON);
+        console.log(listTodos);
+        setListTodos(respuestaJSON);
+        console.log(listTodos);
+      } else {
+        console.log("respuesta fallida");
+        setListTodos([]);
       }
+    } catch {
+      (err) => console.log(err);
     }
-    catch{err=>console.log(err)}
-  }
+  };
 
-  useEffect(()=>{
-    traerListaTareas()
-  },[usuario])
+  const eliminarTarea = async (indiceTarea) => {
+    let URI = `${BASE_URL}user/${usuario}`;
 
- 
+    let auxArr = listTodos.filter((item, index) => {
+      return index !== indiceTarea;
+    });
+
+    console.log(auxArr);
+    if (auxArr.length > 0) {
+      try {
+        let respuesta = await fetch(URI, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(auxArr),
+        });
+
+        if (respuesta.ok) {
+          console.log("Se eliminó exitósamente la tarea");
+          traerListaTareas();
+        } else {
+          console.log("erro");
+        }
+      } catch {
+        (e) => console.log(e);
+      }
+    }else{
+      let respuesta = await fetch(URI, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(auxArr),
+      });
+    }
+  };
+
+  useEffect(() => {
+    traerListaTareas();
+  }, [usuario]);
 
   const deleteTodo = (indiceTarea) => {
     setListTodos((prevState) => {
@@ -79,7 +110,7 @@ export const Todo = () => {
         placeholder="Nombre de usuario"
         onKeyUp={(e) => {
           if (e.keyCode == "13") {
-            setUsuario(e.target.value)
+            setUsuario(e.target.value);
           }
         }}
       />
@@ -100,26 +131,30 @@ export const Todo = () => {
         }}
       />
       <ul>
-        {listTodos.length>0 && listTodos && listTodos!=undefined? listTodos.map((item, indice) => {
-          return (
-            <li
-              className="list-group-item d-flex justify-content-between"
-              key={indice}
-            >
-              {item.label}
-              <span>{JSON.stringify(item.done)}</span>
-              <button
-                type="button"
-                className="btn btn-light"
-                onClick={(e) => {
-                  deleteTodo(indice);
-                }}
+        {listTodos.length > 0 && listTodos && listTodos != undefined ? (
+          listTodos.map((item, indice) => {
+            return (
+              <li
+                className="list-group-item d-flex justify-content-between"
+                key={indice}
               >
-                Eliminar
-              </button>
-            </li>
-          );
-        }):<>No Hay tareas</>}
+                {item.label}
+                <span>{JSON.stringify(item.done)}</span>
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  onClick={(e) => {
+                    eliminarTarea(indice);
+                  }}
+                >
+                  Eliminar
+                </button>
+              </li>
+            );
+          })
+        ) : (
+          <>No Hay tareas</>
+        )}
       </ul>
     </div>
   );
